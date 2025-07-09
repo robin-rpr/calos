8a24178ca4ac932e3bc0f65a8d71bc4278d7c10b
Robin Röper <rr@linux.com>
2025-07-09 23:31:10 +0200

    Improve code quality

--- cmd/net.c
+++ cmd/net.c
@@ -65,71 +65,10 @@
 #define NF_BR_BROUTING     5
 #define NF_BR_NUMHOOKS     6
 
+/* <linux/netfilter_ipv4.h> */
+#define NF_IP_PRE_ROUTING  0
+#define NF_IP_POST_ROUTING 4
 
-#define ARP_HLEN 6
-
-
-/* TODO: All below are not categorized yet. Please categorize! */
-#define NFT_PRIORITY_NAT_POSTROUTING -100
-#define NFT_POLICY_ACCEPT 0
-
-#define NFTNL_EXPR_META_LEN 3
-#define NFTNL_EXPR_META_DEV 4
-
-#ifndef __KERNEL__
-/* IP Cache bits. */
-/* Src IP address. */
-#define NFC_IP_SRC		0x0001
-/* Dest IP address. */
-#define NFC_IP_DST		0x0002
-/* Input device. */
-#define NFC_IP_IF_IN		0x0004
-/* Output device. */
-#define NFC_IP_IF_OUT		0x0008
-/* TOS. */
-#define NFC_IP_TOS		0x0010
-/* Protocol. */
-#define NFC_IP_PROTO		0x0020
-/* IP options. */
-#define NFC_IP_OPTIONS		0x0040
-/* Frag & flags. */
-#define NFC_IP_FRAG		0x0080
-
-/* Per-protocol information: only matters if proto match. */
-/* TCP flags. */
-#define NFC_IP_TCPFLAGS		0x0100
-/* Source port. */
-#define NFC_IP_SRC_PT		0x0200
-/* Dest port. */
-#define NFC_IP_DST_PT		0x0400
-/* Something else about the proto */
-#define NFC_IP_PROTO_UNKNOWN	0x2000
-
-/* IP Hooks */
-/* After promisc drops, checksum checks. */
-#define NF_IP_PRE_ROUTING	0
-/* If the packet is destined for this box. */
-#define NF_IP_LOCAL_IN		1
-/* If the packet is destined for another interface. */
-#define NF_IP_FORWARD		2
-/* Packets coming from a local process. */
-#define NF_IP_LOCAL_OUT		3
-/* Packets about to hit the wire. */
-#define NF_IP_POST_ROUTING	4
-#define NF_IP_NUMHOOKS		5
-#endif /* ! __KERNEL__ */
-
-#ifndef IFLA_BRIDGE_VLAN_INFO
-#define IFLA_BRIDGE_VLAN_INFO 2
-#endif
-
-#ifndef BRIDGE_VLAN_INFO_PVID
-#define BRIDGE_VLAN_INFO_PVID (1 << 1)
-#endif
-
-#ifndef BRIDGE_VLAN_INFO_UNTAGGED
-#define BRIDGE_VLAN_INFO_UNTAGGED (1 << 2)
-#endif
 
 /** Functions **/
 
